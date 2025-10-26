b162c4a428aa04be9122268966e33ba0a806a0ca
Szymon-Srokowski <ss51102@zut.edu.pl>
2025-10-26 15:04:20 +0100

    labD

---
+++ src/Controller/LocationController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Location;
+use App\Form\LocationType;
+use App\Repository\LocationRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/location')]
+final class LocationController extends AbstractController
+{
+    #[Route('', name: 'app_location_index', methods: ['GET'])]
+    public function index(LocationRepository $locationRepository): Response
+    {
+        return $this->render('location/index.html.twig', [
+            'locations' => $locationRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/new', name: 'app_location_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $location = new Location();
+
+
+        $form = $this->createForm(LocationType::class, $location, [
+            'validation_groups' => ['new'],
+        ]);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($location);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Location created.');
+            return $this->redirectToRoute('app_location_index', status: Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('location/new.html.twig', [
+            'location' => $location,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_location_show', methods: ['GET'])]
+    public function show(Location $location): Response
+    {
+        return $this->render('location/show.html.twig', [
+            'location' => $location,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_location_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Location $location, EntityManagerInterface $entityManager): Response
+    {
+
+        $form = $this->createForm(LocationType::class, $location, [
+            'validation_groups' => ['edit'],
+        ]);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Location updated.');
+            return $this->redirectToRoute('app_location_index', status: Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('location/edit.html.twig', [
+            'location' => $location,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_location_delete', methods: ['POST'])]
+    public function delete(Request $request, Location $location, EntityManagerInterface $entityManager): Response
+    {
+
+        $token = $request->getPayload()->getString('_token');
+
+        if ($this->isCsrfTokenValid('delete' . $location->getId(), $token)) {
+            $entityManager->remove($location);
+            $entityManager->flush();
+            $this->addFlash('success', 'Location deleted.');
+        }
+
+        return $this->redirectToRoute('app_location_index', status: Response::HTTP_SEE_OTHER);
+    }
+}
