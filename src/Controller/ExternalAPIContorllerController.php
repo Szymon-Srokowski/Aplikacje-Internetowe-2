4f6ae2b776c9f77208ac36d5fc8f1127cfca1250
Szymon-Srokowski <ss51102@zut.edu.pl>
2025-11-16 15:27:28 +0100

    LAB F

---
+++ src/Controller/ExternalAPIContorllerController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Controller;
+
+use App\Form\ExternalAPIType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class ExternalAPIContorllerController extends AbstractController
+{
+    #[Route('/externalapi', name: 'app_external_api')]
+    public function index(Request $request): Response
+    {
+        $form = $this->createForm(ExternalAPIType::class);
+        $weatherData = null;
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $data = $form->getData();
+
+            $longitude = $data['longitude'];
+            $latitude = $data['latitude'];
+
+
+            $apiUrl = sprintf(
+                'https://api.open-meteo.com/v1/forecast?latitude=%s&longitude=%s&daily=temperature_2m_max,temperature_2m_min,precipitation_sum&timezone=UTC',
+                $latitude,
+                $longitude
+            );
+
+
+            $response = file_get_contents($apiUrl);
+            $weatherData = json_decode($response, true);
+        }
+
+        return $this->render('external_api_contorller/index.html.twig', [
+            'form' => $form->createView(),
+            'weather_data' => $weatherData,
+        ]);
+    }
+}
